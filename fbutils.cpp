adbf7b1ba7a7a9d49fe1d844a3386973b0629797
fabriofabrio <43786221+fabriofabrio@users.noreply.github.com>
2018-10-03 15:24:39 +0200

    aggiunti campi per PhiInAnalog

---
+++ fbutils.cpp
@@ -0,0 +1,181 @@
+#include <Arduino.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <vector>
+
+#include "debug.h"
+#include "fbutils.h"
+#include "pht.h"
+#include "rf.h"
+
+static std::vector<String> RegIDs;
+static std::vector<IoEntry> IoEntryVec;
+static std::vector<ProgEntry> ProgVec;
+
+void FB_deinitRegIDsDB(void) { RegIDs.erase(RegIDs.begin(), RegIDs.end()); }
+
+void FB_addRegIDsDB(String string) {
+  if (RegIDs.size() < NUM_REGIDS_MAX) {
+    RegIDs.push_back(string);
+  }
+}
+
+std::vector<String> &FB_getRegIDs() { return RegIDs; }
+
+void FB_deinitIoEntryDB(void) {
+  IoEntryVec.erase(IoEntryVec.begin(), IoEntryVec.end());
+}
+
+IoEntry &FB_getIoEntry(uint8_t i) { return IoEntryVec[i]; }
+
+uint8_t FB_getIoEntryLen(void) { return IoEntryVec.size(); }
+
+void FB_addIoEntryDB(String key, JsonObject &obj) {
+  if (IoEntryVec.size() < NUM_IO_ENTRY_MAX) {
+    IoEntry entry;
+    entry.key = key;
+    entry.code = obj[F("code")].as<uint8_t>();
+    entry.value = obj[F("value")].as<String>();
+    entry.ioctl = obj[F("ioctl")].as<uint32_t>();
+    entry.enLog = obj[F("enLog")].as<bool>();
+    entry.enWrite = obj[F("drawWr")].as<bool>();
+    entry.enRead = obj[F("drawRd")].as<bool>();
+    entry.enReadAnal = obj[F("drawRdAnal")].as<uint32_t>();
+
+    // TODO: can be done a setup here
+    entry.ev = false;
+    entry.ev_value = 0;
+    entry.wb = false;
+    entry.cb = obj[F("cb")].as<String>();
+
+    // post process data value for some case
+    switch (entry.code) {
+    case kPhyIn: {
+      uint16_t ioctl = entry.ioctl;
+      pinMode(ioctl, INPUT);
+    } break;
+    case kPhyInAnal: {
+      uint16_t ioctl = entry.ioctl;
+      pinMode(ioctl, INPUT);
+    } break;
+
+    case kPhyOut: {
+      uint32_t value = atoi(entry.value.c_str());
+      uint16_t ioctl = entry.ioctl;
+      pinMode(ioctl, OUTPUT);
+      digitalWrite(ioctl, !!value);
+    } break;
+    case kDhtTemperature:
+    case kDhtHumidity: {
+      uint16_t ioctl = entry.ioctl;
+      uint8_t pin = ioctl & 0xFF;
+      uint32_t period = ioctl >> 8;
+      PHT_Set(pin, period);
+    } break;
+    case kRadioRx: {
+      uint16_t ioctl = entry.ioctl;
+      RF_SetRxPin(ioctl);
+    } break;
+    case kRadioTx: {
+      uint16_t ioctl = entry.ioctl;
+      RF_SetTxPin(ioctl);
+    } break;
+    case kBool: {
+      if (entry.value == F("false")) {
+        entry.value = F("0");
+      } else if (entry.value == F("true")) {
+        entry.value = F("1");
+      } else {
+        DEBUG_PRINT("kBool error\n");
+        entry.value = F("0");
+      }
+    } break;
+    default:
+      break;
+    }
+    IoEntryVec.push_back(entry);
+  }
+}
+
+String &FB_getIoEntryNameById(uint8_t i) {
+  IoEntry &entry = IoEntryVec[i];
+  return entry.key;
+}
+
+uint8_t FB_getIoEntryIdx(const char *key) {
+  uint8_t i = 0;
+  uint8_t idx = 0xFF;
+  uint8_t res;
+
+  while ((i < IoEntryVec.size()) && (idx == 0xFF)) {
+    res = strcmp(IoEntryVec[i].key.c_str(), key);
+    if (res == 0) {
+      idx = i;
+    }
+    i++;
+  }
+
+  return idx;
+}
+
+void FB_deinitProgDB(void) { ProgVec.erase(ProgVec.begin(), ProgVec.end()); }
+
+ProgEntry &FB_getProg(uint8_t i) { return ProgVec[i]; }
+
+uint8_t FB_getProgLen(void) { return ProgVec.size(); }
+
+void FB_addProgDB(String key, JsonObject &obj) {
+  ProgEntry entry;
+  entry.key = key;
+  DEBUG_PRINT("FB_addProgDB: key=%s\n", entry.key.c_str());
+
+  JsonArray &nest = obj[F("p")].as<JsonArray>();
+  for (uint32_t i = 0; i < nest.size(); ++i) {
+    FuncEntry fentry;
+    fentry.code = nest[i][F("i")].as<int>();
+    if (nest[i][F("v")].as<String>()) {
+      fentry.value = nest[i][F("v")].as<String>();
+    }
+    entry.funcvec.push_back(fentry);
+  }
+  ProgVec.push_back(entry);
+}
+
+uint8_t FB_getProgIdx(const char *key) {
+  uint8_t i = 0;
+  uint8_t idx = 0xFF;
+  uint8_t res;
+
+  while ((i < ProgVec.size()) && (idx == 0xFF)) {
+    res = strcmp(ProgVec[i].key.c_str(), key);
+    if (res == 0) {
+      idx = i;
+    }
+    i++;
+  }
+  return idx;
+}
+
+void FB_dumpIoEntry(void) {
+  DEBUG_PRINT("FB_dumpIoEntry\n");
+  for (uint8_t i = 0; i < IoEntryVec.size(); ++i) {
+    DEBUG_PRINT(
+        "%d: key=%s, code=%d, value=%s, ioctl=%x, ev=%d, ev_value=%d, cb=%s\n",
+        i, IoEntryVec[i].key.c_str(), IoEntryVec[i].code,
+        IoEntryVec[i].value.c_str(), IoEntryVec[i].ioctl, IoEntryVec[i].ev,
+        IoEntryVec[i].ev_value, IoEntryVec[i].cb.c_str());
+  }
+}
+
+void FB_dumpProg(void) {
+  DEBUG_PRINT("FB_dumpProg\n");
+  for (uint8_t i = 0; i < ProgVec.size(); ++i) {
+    DEBUG_PRINT("%d: key=%s\n", i, ProgVec[i].key.c_str());
+    for (uint8_t j = 0; j < ProgVec[i].funcvec.size(); j++) {
+      DEBUG_PRINT("  %d: code=%d, value=%s\n", j, ProgVec[i].funcvec[j].code,
+                  ProgVec[i].funcvec[j].value.c_str());
+    }
+  }
+}
---
+++ vm.cpp
@@ -0,0 +1,323 @@
+#include <Arduino.h>
+
+#include <stdio.h>
+#include <string.h>
+
+#include "debug.h"
+#include "fbconf.h"
+#include "fblog.h"
+#include "fbm.h"
+#include "fbutils.h"
+#include "firebase.h"
+#include "pht.h"
+#include "rf.h"
+#include "timers.h"
+#include "timesrv.h"
+#include "vmasm.h"
+
+bool VM_UpdateDataPending;
+void VM_UpdateDataReq(void) { VM_UpdateDataPending = true; }
+
+void VM_readIn(void) {
+  RF_Service();
+  PHT_Service();
+  Timers_Service();
+  bool UpdateDataFault = false;
+
+  /* loop over data elements looking for events */
+  for (uint8_t id = 0; id < FB_getIoEntryLen(); id++) {
+    IoEntry &entry = FB_getIoEntry(id);
+    switch (entry.code) {
+    case kPhyIn: {
+      uint32_t v = atoi(entry.value.c_str());
+      uint8_t pin = entry.ioctl;
+      pinMode(pin, INPUT);
+      uint32_t value = digitalRead(pin);
+      if (v != value) {
+        DEBUG_PRINT("VM_readIn: %s, %d, %d\n", entry.key.c_str(), value, v);
+        entry.value = value;
+        entry.ev = true;
+        entry.ev_value = value;
+        entry.wb = true;
+        entry.wblog = true;
+      }
+    } break;
+case kPhyInAnal: {
+      uint32_t v = atoi(entry.value.c_str());
+      uint8_t pin = entry.ioctl;
+      pinMode(pin, INPUT);
+      uint32_t value = analogRead(pin);
+      if (v != value) {
+        DEBUG_PRINT("VM_readInAnalog: %s, %d, %d\n", entry.key.c_str(), value, v);
+        entry.value = value;
+        entry.ev = true;
+        entry.ev_value = value;
+        entry.wb = true;
+        entry.wblog = true;
+      }
+    } break;
+
+    case kPhyOut: {
+      if ((VM_UpdateDataPending == true) && (entry.enWrite == true)) {
+        DEBUG_PRINT("get: kPhyOut %s\n", entry.key.c_str());
+        String kdata;
+        FbSetPath_data(kdata);
+        uint32_t value =
+            Firebase.getInt(kdata + F("/") + entry.key + F("/value"));
+        if (Firebase.failed() == true) {
+          DEBUG_PRINT("get failed: kPhyOut %s\n", entry.key.c_str());
+        } else {
+          uint32_t v = atoi(entry.value.c_str());
+          if (v != value) {
+            DEBUG_PRINT("VM_readIn: %s, %d, %d\n", entry.key.c_str(), value, v);
+            entry.value = value;
+            entry.ev = true;
+            entry.ev_value = value;
+            entry.wb = true;
+          }
+        }
+      }
+    } break;
+    case kBool: {
+      if ((VM_UpdateDataPending == true) && (entry.enWrite == true)) {
+        DEBUG_PRINT("get: kBool %s\n", entry.key.c_str());
+        String kdata;
+        FbSetPath_data(kdata);
+        bool value = Firebase.getBool(kdata + F("/") + entry.key + F("/value"));
+        if (Firebase.failed() == true) {
+          DEBUG_PRINT("get failed: kBool %s\n", entry.key.c_str());
+          UpdateDataFault = true;
+        } else {
+          uint32_t v = atoi(entry.value.c_str());
+          if (v != value) {
+            DEBUG_PRINT("VM_readIn: %s, %d\n", entry.key.c_str(), value);
+            entry.value = value;
+            entry.ev = true;
+            entry.ev_value = value;
+            entry.wblog = true;
+          }
+        }
+      }
+    } break;
+    case kInt: {
+      if ((VM_UpdateDataPending == true) && (entry.enWrite == true)) {
+        DEBUG_PRINT("get: kInt %s\n", entry.key.c_str());
+        String kdata;
+        FbSetPath_data(kdata);
+        uint32_t value =
+            Firebase.getInt(kdata + F("/") + entry.key + F("/value"));
+        if (Firebase.failed() == true) {
+          DEBUG_PRINT("get failed: kInt %s\n", entry.key.c_str());
+          UpdateDataFault = true;
+        } else {
+          uint32_t v = atoi(entry.value.c_str());
+          if (v != value) {
+            DEBUG_PRINT("VM_readIn: %s, %d\n", entry.key.c_str(), value);
+            entry.value = value;
+            entry.ev = true;
+            entry.ev_value = value;
+            entry.wblog = true;
+          }
+        }
+      }
+    } break;
+    default:
+      // DEBUG_PRINT("VM_readIn: error\n");
+      break;
+    }
+  }
+  VM_UpdateDataPending = UpdateDataFault;
+}
+
+void VM_writeOutMessage(vm_context_t &ctx, String value) {
+  DEBUG_PRINT("VM_writeOutMessage: %s\n", value.c_str());
+  String message = value + F(" ") + ctx.ev_name;
+  fblog_log(message, true);
+}
+
+void VM_writeOut(void) {
+  /* loop over data elements looking for write-back requests */
+  for (uint8_t i = 0; i < FB_getIoEntryLen(); i++) {
+    IoEntry &entry = FB_getIoEntry(i);
+    if (entry.wb == true) {
+      switch (entry.code) {
+      case kPhyOut: {
+        uint32_t v = atoi(entry.value.c_str());
+        uint8_t pin = entry.ioctl;
+        pinMode(pin, OUTPUT);
+        digitalWrite(pin, v);
+        if (entry.enRead == true) {
+          DEBUG_PRINT("VM_writeOut: %s: %d\n", entry.key.c_str(), v);
+          String ref;
+          FbSetPath_data(ref);
+          Firebase.setInt(ref + F("/") + entry.key + F("/value"), v);
+          if (Firebase.failed() == true) {
+            DEBUG_PRINT("Firebase set failed: VM_writeOut %s\n",
+                        entry.key.c_str());
+          } else {
+            entry.wb = false;
+          }
+        } else {
+          entry.wb = false;
+        }
+      } break;
+      case kRadioTx: {
+        uint32_t v = atoi(entry.value.c_str());
+        RF_Send(v, 24);
+        entry.wb = false;
+      } break;
+      case kPhyIn:
+      case kRadioIn:
+      case kRadioRx:
+      case kInt: {
+        if (entry.enRead == true) {
+          uint32_t v = atoi(entry.value.c_str());
+          DEBUG_PRINT("VM_writeOut: %s: %d\n", entry.key.c_str(), v);
+          String ref;
+          FbSetPath_data(ref);
+          Firebase.setInt(ref + F("/") + entry.key + F("/value"), v);
+          if (Firebase.failed() == true) {
+            DEBUG_PRINT("Firebase set failed: VM_writeOut %s\n",
+                        entry.key.c_str());
+          } else {
+            if ((entry.enLog == true) && (entry.wblog == true)) {
+              DynamicJsonBuffer jsonBuffer;
+              JsonObject &json = jsonBuffer.createObject();
+              json[F("t")] = getTime();
+              json[F("v")] = v;
+              String strdata;
+              json.printTo(strdata);
+              FbSetPath_log(ref);
+              DEBUG_PRINT("VM_writeOut-log: %s: %d\n", entry.key.c_str(), v);
+              Firebase.pushJSON(ref + F("/") + entry.key, strdata);
+              if (Firebase.failed() == true) {
+                DEBUG_PRINT("Firebase push failed: VM_writeOut %s\n",
+                            entry.key.c_str());
+              } else {
+                entry.wb = false;
+                entry.wblog = false;
+              }
+            } else {
+              entry.wb = false;
+            }
+          }
+        }
+      } break;
+      case kDhtTemperature:
+      case kDhtHumidity:
+      case kFloat: {
+        if (entry.enRead == true) {
+          float v = atof(entry.value.c_str());
+          DEBUG_PRINT("VM_writeOut: %s: %f\n", entry.key.c_str(), v);
+          String ref;
+          FbSetPath_data(ref);
+          Firebase.setFloat(ref + F("/") + entry.key + F("/value"), v);
+          if (Firebase.failed() == true) {
+            DEBUG_PRINT("Firebase set failed: VM_writeOut %s\n",
+                        entry.key.c_str());
+          } else {
+            if ((entry.enLog == true) && (entry.wblog == true)) {
+              DynamicJsonBuffer jsonBuffer;
+              JsonObject &json = jsonBuffer.createObject();
+              json[F("t")] = getTime();
+              json[F("v")] = v;
+              String strdata;
+              json.printTo(strdata);
+              FbSetPath_log(ref);
+              DEBUG_PRINT("VM_writeOut-log: %s: %f\n", entry.key.c_str(), v);
+              Firebase.pushJSON(ref + F("/") + entry.key, strdata);
+              if (Firebase.failed() == true) {
+                DEBUG_PRINT("Firebase push failed: VM_writeOut %s\n",
+                            entry.key.c_str());
+              } else {
+                entry.wb = false;
+                entry.wblog = false;
+              }
+            } else {
+              entry.wb = false;
+            }
+          }
+        }
+      } break;
+      case kBool: {
+        if (entry.enRead == true) {
+          bool v = atoi(entry.value.c_str());
+          DEBUG_PRINT("VM_writeOut: %s: %d\n", entry.key.c_str(), v);
+          String ref;
+          FbSetPath_data(ref);
+          Firebase.setBool(ref + F("/") + entry.key + F("/value"), v);
+          if (Firebase.failed() == true) {
+            DEBUG_PRINT("Firebase set failed: VM_writeOut %s\n",
+                        entry.key.c_str());
+          } else {
+            entry.wb = false;
+          }
+        } else {
+          entry.wb = false;
+        }
+      } break;
+      case kMessaging: {
+        DEBUG_PRINT("VM_writeOut: kMessaging %s\n", entry.value.c_str());
+        fblog_log(entry.value, true);
+        entry.wb = false;
+      } break;
+      default:
+        // DEBUG_PRINT("VM_writeOut: error\n");
+        break;
+      }
+    }
+  }
+}
+
+void VM_run(void) {
+  // update inputs
+  VM_readIn();
+
+  /* loop over data elements looking for event notifications */
+  for (uint8_t i = 0; i < FB_getIoEntryLen(); i++) {
+    IoEntry &entry = FB_getIoEntry(i);
+
+    if (entry.ev == true) {
+      entry.ev = false;
+      String cbkey = entry.cb;
+
+      DEBUG_PRINT("event found on: %s\n", entry.key.c_str());
+
+      // DEBUG_PRINT("cbkey: %s\n", cbkey.c_str());
+      if (cbkey.length() != 0) {
+        vm_context_t ctx;
+
+        /* init ACC with event value */
+        ctx.V = 0;
+        ctx.ACC = entry.ev_value;
+        ctx.HALT = false;
+
+        /* keep the event name */
+        ctx.ev_name = entry.key.c_str();
+
+        uint8_t id_prog = FB_getProgIdx(cbkey.c_str());
+        ProgEntry &prog = FB_getProg(id_prog);
+        std::vector<FuncEntry> &funcvec = prog.funcvec;
+
+        DEBUG_PRINT("VM_run start [%s] >>>>>>>>>>>>\n", prog.key.c_str());
+        DEBUG_PRINT("Heap: %d\n", ESP.getFreeHeap());
+
+        uint8_t pc = 0;
+        while ((pc < funcvec.size()) && (ctx.HALT == false)) {
+          DEBUG_PRINT("VM_run start [%d] code=%d, ACC=%d V=%d\n", pc,
+                      funcvec[pc].code, ctx.ACC, ctx.V);
+
+          /* decode */
+          pc = VM_decode(pc, ctx, funcvec[pc]);
+
+          DEBUG_PRINT("VM_run stop [%d] code=%d, ACC=%d V=%d\n", pc,
+                      funcvec[pc].code, ctx.ACC, ctx.V);
+        }
+        DEBUG_PRINT("VM_run stop [%s] <<<<<<<<<<<<\n", prog.key.c_str());
+      }
+    }
+  }
+
+  // update outputs
+  VM_writeOut();
+}
